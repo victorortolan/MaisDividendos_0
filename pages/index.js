3bc1238673ef66a9d7b8b794c406b61ef0c47e22
alvescode <videoliva@gmail.com>
2024-04-23 13:51:24 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Home = () =>{
+    
+    
+    return(
+        <div>
+            <h1>Vítor Alves</h1>
+            <h2>Desenvolvedor</h2>
+        </div>
+        )
+}
+
+export default Home;
