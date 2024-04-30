9de4e20809ffc63124ff319c27fbb105b3818ebc
alvescode <videoliva@gmail.com>
2024-04-30 17:40:46 -0300

    atualiza a home

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,22 +7,38 @@ const Header = () => {
   const router = useRouter();
 
   const ActiveLink = ({ children, href }) => {
-    const style = {
-      color: router.pathname == href ? "#6ead3a" : "white",
-    };
+    // const style = {
+    //   color: router.pathname == href ? "#6ead3a" : "white",
+    // };
     return (
-      <Link href={href} style={style}>
-        {children}
-      </Link>
+      <div className={styles.link}>
+        <Link href={href} className={styles.a_link} target="_blank">
+          {children}
+        </Link>
+      </div>
     );
   };
 
   return (
     <header className={styles.header}>
-      <ActiveLink href="/">Home</ActiveLink>
-      <ActiveLink href="/projetos">Projetos</ActiveLink>
-      <ActiveLink href="/sobre">Sobre esse Projeto</ActiveLink>
-      <ActiveLink href="/eu">Sobre Mim</ActiveLink>
+      <h1 className={styles.title}>Vítor Alves</h1>
+      <h2 className={styles.subtitle}>Desenvolvedor</h2>
+      <p className={styles.header_p}>
+        Estudando Ciência da Computação, Tecnologia e Desenvolvimento.
+      </p>
+      <ul
+        style={{
+          "margin-top": "16px",
+          display: "flex",
+          "flex-direction": "row",
+        }}
+      >
+        <ActiveLink href="https://github.com/alvescode">GitHub</ActiveLink>
+        <ActiveLink href="https://www.linkedin.com/in/alvescode/">
+          LinkedIn
+        </ActiveLink>
+        <ActiveLink href="/projetos">Envie-me Uma Mensagem Aqui!</ActiveLink>
+      </ul>
     </header>
   );
 };
