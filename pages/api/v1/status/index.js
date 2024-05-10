5e1d758a5254f6d7ea658d8de9d8058ca33097b6
alvescode <videoliva@gmail.com>
2024-05-10 12:56:18 -0300

    add database.js and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  res.status(200).json({ resposta: "tudo certo por aqui" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows[0].sum);
+  response.status(200).json({ resposta: "tudo certo por aqui" });
 }
 
 export default status;
